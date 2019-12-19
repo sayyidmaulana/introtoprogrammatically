2eb1d6ea3853cd988ff9488e9464e85454b4ca03
Sayyid Maulana Khakul Yakin <sayyid.yakin@students.amikom.ac.id>
2019-12-19 19:34:36 +0700

    Initial Commit

---
+++ ProgrammaticPageControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammaticPageControl
+//
+//  Created by sayyid maulana khakul yakin on 19/12/19.
+//  Copyright © 2019 sayyid maulana khakul yakin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
