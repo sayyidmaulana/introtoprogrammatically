5cb0778e5fda590ca9f2e436e722aed341f7d5fc
Sayyid Maulana Khakul Yakin <sayyidmaulanakhakulyakin@Sayyids-MacBook-Pro.local>
2021-02-16 10:22:37 +0700

    feat(Pesan) insert background XLPager pesan view

--- ProgrammaticPageControl/TopTabBarProgrammatic/TopTabBarMain.swift
+++ ProgrammaticPageControl/TopTabBarProgrammatic/TopTabBarMain.swift
@@ -13,19 +13,19 @@ class TopTabBarMain: ButtonBarPagerTabStripViewController {
     let purpleInspireColor = UIColor(red:0.13, green:0.03, blue:0.25, alpha:1.0)
 
     override func viewDidLoad() {
+        // change selected bar color
+        settings.style.buttonBarBackgroundColor = .white
+        settings.style.buttonBarItemBackgroundColor = .white
+        settings.style.selectedBarBackgroundColor = purpleInspireColor
+        settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+        settings.style.selectedBarHeight = 2.0
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemTitleColor = .black
+        settings.style.buttonBarItemsShouldFillAvailableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+        settings.style.buttonBarItemLeftRightMargin = 0
         super.viewDidLoad()
-                // change selected bar color
-                settings.style.buttonBarBackgroundColor = .white
-                settings.style.buttonBarItemBackgroundColor = .white
-                settings.style.selectedBarBackgroundColor = purpleInspireColor
-                settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
-                settings.style.selectedBarHeight = 2.0
-                settings.style.buttonBarMinimumLineSpacing = 0
-                settings.style.buttonBarItemTitleColor = .black
-                settings.style.buttonBarItemsShouldFillAvailableWidth = true
-                settings.style.buttonBarLeftContentInset = 0
-                settings.style.buttonBarRightContentInset = 0
-                settings.style.buttonBarItemLeftRightMargin = 0
 
                 changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
                 guard changeCurrentIndex == true else { return }
